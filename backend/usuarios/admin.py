bff65f5a236ce0728e2caef4315975ebebc5daaa
Bellcebu <alejandroku001@gmail.com>
2025-07-09 23:12:12 -0300

    feat: model de usuarios y admin.py del mismo

--- backend/usuarios/admin.py
+++ backend/usuarios/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        ('Rol de usuario', {'fields': ('rol',)}),
+    )
+    list_display = ('username', 'email', 'rol', 'is_staff')
+
+admin.site.register(User, CustomUserAdmin)
