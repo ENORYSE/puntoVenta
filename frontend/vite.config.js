5b4f3ce0fa0fdfd1b6faff1243841705f3cfe872
felix-op <felixoscarperezsegovia@gmail.com>
2025-07-09 21:30:59 -0300

    feat: se agrego un alias para pages

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@pages': '/src/pages',
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
